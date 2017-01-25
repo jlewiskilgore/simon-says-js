21b425bf6c53dd0f103e150ef0e9391c07b10a08
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-24 19:24:50 -0500

    Generate 20 button sequence and make game press first button

--- js/simon.js
+++ js/simon.js
@@ -1,3 +1,35 @@
+var currGameSequence;
+var numRight;
+startGame();
+console.log(currGameSequence);
+
+function startGame() {
+	currGameSequence = []; // Clear sequence for the new game
+	numRight = 0;
+	currGameSequence = generateRandomSequence(20);
+
+	nextTurn();
+}
+
+function nextTurn() {
+	var currentButton = currGameSequence[numRight];
+
+	switch(currentButton) {
+		case 0:
+			pressColorButton('green');
+			break;
+		case 1:
+			pressColorButton('red');
+			break;
+		case 2:
+			pressColorButton('yellow');
+			break;
+		case 3:
+			pressColorButton('blue');
+			break;
+	}
+}
+
 function pressColorButton(color) {
 	var audioFile = "sounds/";
 	switch(color) {
@@ -16,4 +48,23 @@ function pressColorButton(color) {
 	}
 	var audio = new Audio(audioFile);
 	audio.play();
+}
+
+/* 
+* Generate an array of numElements random numbers 0 to 3 where:
+* 0 = Green
+* 1 = Red
+* 2 = Yellow
+* 3 = Blue
+*/
+function generateRandomSequence(numElements) {
+	var sequence = [];
+	var nextNum;
+
+	for(var i = 0; i < numElements; i++) {
+		nextNum = Math.floor(Math.random() * 4);
+		sequence.push(nextNum);
+	}
+
+	return sequence;
 }
